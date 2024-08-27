8a2bda9ba34220292831b25afc92996f217c3731
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-27 19:55:19 +0300

    frist

---
+++ get_it_assignment/lib/core/all_file.dart
@@ -0,0 +1,4 @@
+export '../screen/views/account_view.dart';
+export '../screen/views/main_view.dart';
+export '../screen/views/record_view.dart';
+export '../screen/home_screen.dart';
---
+++ get_it_assignment/lib/screen/views/record_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RecordView extends StatelessWidget {
+  const RecordView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
