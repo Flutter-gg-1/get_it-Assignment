ac8c22d32c0af779581854e40c8952b03137a51d
Najwa-aldahri <aldahri.najwa@gmail.com>
2024-08-28 21:55:51 +0300

    done with the load

--- get_assignment/lib/data_layer/tweets_data.dart
+++ get_assignment/lib/data_layer/tweets_data.dart
@@ -1,22 +1,39 @@
+import 'dart:math';
+
 import 'package:get_assignment/models/task_model.dart';
+import 'package:get_assignment/widget/tweet_sizebox.dart';
 import 'package:get_storage/get_storage.dart';
 
 class TweetsData {
-  List<TweetModel> allTwets = [
-    TweetModel(id: 1, title: "good morning every one"),
-    TweetModel(id: 2, title: "i am happy today and every day")
-  ];
+  List<TweetModel> allTwets = [];
 
   final box = GetStorage();
+  TweetsData() {
+    loadTweets();
+  }
 
   // List<String> allTwets2 = [];
 
   addNewTweet({required String tweet}) {
-    allTwets.add(TweetModel(id: 3, title: tweet));
+    int id = Random().nextInt(1000);
+    while (allTwets.map((e) => e.id).toList().contains(id)) {
+      id = id = Random().nextInt(1000);
+    }
+    allTwets.add(TweetModel(id: id, title: tweet));
     saveTweet();
   }
 
   saveTweet() async {
     await box.write("tweet", allTwets);
+  }
+
+  loadTweets() {
+    if (box.hasData("tweet")) {
+      List<Map<String, dynamic>> TweetsAsJson =
+          List.from(box.read("tweet")).cast<Map<String, dynamic>>();
+      for (var element in TweetsAsJson) {
+        allTwets.add(TweetModel.fromJson(element));
+      }
+    }
   }
 }
