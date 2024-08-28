bb77d4d0dbe36df7fe04e811ee1c7357a0cbf95e
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-28 13:01:02 +0300

    New

---
+++ get_it_assignment/android/app/src/main/kotlin/com/example/get_it_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.get_it_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ get_it_assignment/lib/core/all_file.dart
@@ -0,0 +1,2 @@
+export '../screen/home_screen.dart';
+export '../screen/page/main_screen.dart';
