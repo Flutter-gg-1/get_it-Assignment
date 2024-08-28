f83978d2324839ddb903543da2b4dc3a0d0cf171
Abdullah Alshammari <Abu.Mukhlef@gmail.com>
2024-08-28 07:04:30 +0300

    UPD BottomNavigationBar version 0.0.0

--- get_it_assignment/lib/screen/home_screen.dart
+++ get_it_assignment/lib/screen/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,6 +43,14 @@ class _HomeScreenState extends State<HomeScreen> {
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color.fromARGB(255, 124, 183, 126),
         selectedIconTheme: const IconThemeData(color: Colors.white),
+        unselectedIconTheme:
+            const IconThemeData(color: Color.fromARGB(255, 186, 175, 175)),
+        selectedFontSize: 16,
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          _selectedIndex = index;
+          setState(() {});
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
