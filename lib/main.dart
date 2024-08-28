c4a6480461bb37abce0a03cf6592951ee0c68f94
FaHaDoF69 <93477293+FaHaDoF69@users.noreply.github.com>
2024-08-29 02:11:11 +0300

    add setup function in main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:get_it_assignment/src/screens/home_screen.dart';
+import 'package:get_it_assignment/src/services/setup.dart';
 
-void main() {
+void main() async {
+  // To ensure Flutter is initialized before running the app
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // call setup function to initialize services and dependencies
+  await setup();
   runApp(const MainApp());
 }
 
