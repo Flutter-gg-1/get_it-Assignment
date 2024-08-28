ff4144ee4dd4e9a0c14f07603f6c74c770d1a609
Yaser Alkhayyat <yaserkhayyat2017@gmail.com>
2024-08-28 19:39:36 +0300

    finish second page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:get_it_assignment/extensions/screen_push.dart';
 import 'package:get_it_assignment/extensions/screen_size.dart';
+import 'package:get_it_assignment/screens/tweet_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,16 +19,23 @@ class HomeScreen extends StatelessWidget {
         child: AppBar(
           centerTitle: true,
           bottom: PreferredSize(preferredSize: Size(context.getWidth(), double.minPositive), child: const Divider(color: Colors.black26,)),
-          title: const FaIcon(FontAwesomeIcons.twitter, color: Color(0xff4C9EEB),) ,
+          title: Image.asset('assets/twitter_logo.png', width: 27, height: 22,) ,
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xff4c9eeb),
-        shape: const OvalBorder(),
-        onPressed: (){
-          // context.push(target: target, saveData: )
-        },
-        child: Image.asset('assets/new_tweet.png', scale: 2,)
+      floatingActionButton: Container(
+        margin: const EdgeInsets.only(bottom: 95),
+        child: FloatingActionButton(
+          backgroundColor: const Color(0xff4c9eeb),
+          shape: const OvalBorder(),
+          onPressed: (){
+            context.push(target: const TweetScreen(), saveData: (p0) {
+              if(p0==true) {
+                setState(() {});
+              }
+            });
+          },
+          child: Image.asset('assets/new_tweet.png', scale: 2,)
+        ),
       ),
     );
   }
---
+++ lib/screens/tweet_screen.dart
@@ -0,0 +1,78 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:get_it_assignment/extensions/screen_size.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class TweetScreen extends StatefulWidget {
+  
+  const TweetScreen({super.key});
+
+  @override
+  State<TweetScreen> createState() => _TweetScreenState();
+}
+
+class _TweetScreenState extends State<TweetScreen> {
+  
+  TextEditingController tweetController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size(context.getWidth(), 59),
+        child: AppBar(
+          centerTitle: true,
+          bottom: PreferredSize(preferredSize: Size(context.getWidth(), double.minPositive), child: const Divider(color: Colors.black26,)),
+          title: Image.asset('assets/twitter_logo.png', width: 27, height: 22,) ,
+        ),
+      ),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextField(
+                    onTapOutside: (event) => FocusManager.instance.primaryFocus?.unfocus(),
+                    controller: tweetController,
+                    minLines: 1,
+                    maxLines: 5,
+                    maxLength: 280,
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      counterText: "",
+                      label: Text("What is happening?", style: GoogleFonts.actor(fontSize: 16),),
+                      filled: true,
+                      fillColor: Color(0xffE7ECF0),
+                      constraints: BoxConstraints(minHeight: 35, maxHeight: 35*3, maxWidth: 280)
+                    ),
+                  ),
+                  IconButton(onPressed: (){
+                    log(tweetController.text);
+                  }, icon: Icon(Icons.send))
+                ],
+              ),
+              SizedBox(height: 32,),
+              Container(
+                width: context.getWidth(),
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                margin: EdgeInsets.only(bottom: 42),
+                child: TextButton(
+                  style: ButtonStyle(backgroundColor: WidgetStatePropertyAll(Color(0xffE7ECF0))),
+                  onPressed: () => Navigator.pop(context),
+                  child: Text(
+                    "Cancel",
+                    style: GoogleFonts.actor(fontSize: 19, color: Colors.black)
+                  )
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
